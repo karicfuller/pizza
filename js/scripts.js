d61e63ef76205781a3d22094a94ad60a664ed6f5
Kari Fuller <kari.c.fuller@gmail.com>
2018-07-05 18:05:31 -0700

    set up js business logic

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+//Business Logic
+
+function Pizza(size, meat, topping) {
+  this.size = size;
+  this.meat = meat;
+  this.topping = topping;
+  this.price;
+}
+
+Pizza.prototype.calculatePrice = function() {
+  this.price = 8; //size small price
+  if (this.size === "medium") {
+    this.price += 2;
+  } else if (this.size === "large") {
+    this.price += 4;
+  }
+
+//meat price
+  for (var i = 0; i < this.meat.length; i++) {
+  this.price += 2;
+  }
+  //topping price
+  for (var i = 0; i < this.topping.length; i++) {
+  this.price += 1;
+  }
+}
+
+Pizza.prototype.pizzaDescription = function() {
+  return "You have ordered a " + this.size + " " + this.meat + this.topping "pizza: $" + this.price;
+}
+
+var displayPizzaInfo = function() {
+  $("#final_details").show();
+  $("#final_details h5").text(thePizza.pizzaDescription());
+  $("#meat").text(thePizza.meat.join(", "));
+  $("#topping").text(thePizza.topping.join(", "));
+}
