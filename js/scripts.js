bbf591b0feaaf56350287eff8ade5d6b5decf505
Kari Fuller <kari.c.fuller@gmail.com>
2018-07-11 20:56:34 -0700

    updated business and front end logic - does not work

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,55 @@
 //Business Logic
 
-function Pizza(size, meat, topping) {
+function Pizza(size, meat, topping, price) {
   this.size = size;
   this.meat = meat;
   this.topping = topping;
-  this.price;
+  this.price = 0;
 }
 
 Pizza.prototype.calculatePrice = function() {
-  this.price = 8; //base - size small price
-  if (this.size === "medium") {
-    this.price += 2;
-  } else if (this.size === "large") {
-    this.price += 4;
+  if (this.size === "Small") {
+    this.price = this.price + 8;
   }
-
-//meat price
+  else if (this.size === "Medium") {
+    this.price= this.price + 10;
+  }
+  else if (this.size === "Large") {
+    this.price = this.price + 12;
+  }
+  //price of meat
   for (var i = 0; i < this.meat.length; i++) {
   this.price += 2;
   }
-  //topping price
+  //price of toppings
   for (var i = 0; i < this.topping.length; i++) {
   this.price += 1;
   }
-}
+};
 
-Pizza.prototype.pizzaDescription = function() {
-  alert "You have ordered a " + this.size + " " + this.meat + this.topping "pizza for $" + this.price;
-}
-
-
-//UI Logic
+//Front end logic
 
 $(document).ready(function() {
   $("#pizza-form").submit(function(event) {
     event.preventDefault();
     //form inputs
-    var size = $("input[name=size]:checked").val(); {
-      return this.value;
-    })
-    var meat = $("input[name=meat]:checked").val(); {
-      return this.value;
-    })
-    var topping = $("input[name=topping]:checked").val() {
-      return this.value;
-    })
+    var size = $("input:radio[name=size]:checked").val(); {
+      return this.price;
+    }
+    var meat = $("input:checkbox[name=meat]:checked").val(); {
+      return this.price;
+    }
+    var topping = $("input:checkbox[name=topping]:checked").val() {
+      return this.price;
+    }
 
     var thePizza = new Pizza(size, meat, topping);
     //calculate price from input by user
     thePizza.calculatePrice();
+    $("#show-price").text(thePizza.price);
+    $("#results").show();
 
-    //reset to default
     //document.getElementById("pizza-form").reset();
-
-    });
-  });
+  //});
 
 });
